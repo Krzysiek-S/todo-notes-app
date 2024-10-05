e0c82af0460a4f687dc428ed673d87aec39a305e
unknown <Maxon@Vostro3490-1.Maxon.local>
2024-10-05 23:45:00 +0200

    vv

--- app/utils/supabaseClient.js
+++ app/utils/supabaseClient.js
@@ -7,7 +7,7 @@ export const CreateSupabaseClient = (supabaseAccessToken) => {
 
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_KEY,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     {
       global: {
         headers: {
