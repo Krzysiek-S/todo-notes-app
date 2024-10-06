af7ba11a8d0fdb1a11416bbc85fd757622510dbd
unknown <Maxon@Vostro3490-1.Maxon.local>
2024-10-06 19:19:54 +0200

    vv

--- app/utils/supabaseClient.js
+++ app/utils/supabaseClient.js
@@ -4,8 +4,8 @@ export const CreateSupabaseClient = (supabaseAccessToken) => {
   if (!supabaseAccessToken) {
     throw new Error("No Supabase access token provided");
   }
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  const supabaseUrl = import.meta.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
   return createClient(
     supabaseUrl,
