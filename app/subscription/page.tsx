9a8b76381885999817f4342218f66b96233a8634
unknown <Maxon@Vostro3490-1.Maxon.local>
2024-10-10 16:05:48 +0200

    dd

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -91,7 +91,7 @@ export default function SubscriptionPage() {
             </div>
             <p>Stay focused with zero interruptions.</p>
           </li>
-          <div className="border border-gray-500 border-dotted w-2"></div>
+          <div className="border border-gray-500 border-dotted w-[20px]"></div>
           <li className="mb-2">
             <div className="flex justify-center items-center">
               <span className="inline-block w-3 h-3 mr-2 rounded-full bg-[#F76201]"></span>
