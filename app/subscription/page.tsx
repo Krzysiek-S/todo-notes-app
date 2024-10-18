b79e368cf2ff2b23d1d1466c30463888ad778545
unknown <Maxon@Vostro3490-1.Maxon.local>
2024-10-18 15:54:38 +0200

    ff

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -43,7 +43,7 @@ export default function SubscriptionPage() {
 
     if (res.ok) {
       alert("5-dniowy okres próbny rozpoczęty!"); // Możesz zastąpić alert czymś innym, np. UI powiadomieniem
-      await router.push("/"); // Powrót do strony głównej
+      router.push("/"); // Powrót do strony głównej
     } else {
       alert("Błąd podczas rozpoczynania okresu próbnego.");
     }
